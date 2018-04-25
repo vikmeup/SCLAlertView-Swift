de579dc3d0b1217074796dcb5015bef01aaf8a1b
douglas-srs <douglas.srs12@gmail.com>
2018-04-25 14:20:01 -0300

    Update SCLAlertViewPropertiesTests.swift (#403)
    Fix carthage

--- SCLAlertViewTests/SCLAlertViewPropertiesTests.swift
+++ SCLAlertViewTests/SCLAlertViewPropertiesTests.swift
@@ -42,10 +42,6 @@ class SCLAlertViewPropertiesTests: XCTestCase {
         XCTAssertTrue(alert.appearance.kCircleIconHeight == 20.0)
     }
     
-    func testSCLAlertViewTitleTop() {
-        XCTAssertTrue(alert.appearance.kTitleTop == 30.0)
-    }
-    
     func testSCLAlertViewTitleHeight() {
         XCTAssertTrue(alert.appearance.kTitleHeight == 40.0)
     }
