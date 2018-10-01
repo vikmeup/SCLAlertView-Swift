44f1fe90498d0cddc2300b9b6f2621f75db1c120
Matthew Dutton <mduttondev@gmail.com>
2018-10-01 07:25:13 -0400

    return to .normal state

--- SCLAlertView/SCLAlertView.swift
+++ SCLAlertView/SCLAlertView.swift
@@ -590,7 +590,7 @@ open class SCLAlertView: UIViewController {
         // Add button
         let btn = SCLButton()
         btn.layer.masksToBounds = true
-        btn.setTitle(title, for: UIControl.State())
+        btn.setTitle(title, for: .normal)
         btn.titleLabel?.font = appearance.kButtonFont
         btn.customBackgroundColor = backgroundColor
         btn.customTextColor = textColor
@@ -872,10 +872,10 @@ open class SCLAlertView: UIViewController {
             
             if let customTextColor = btn.customTextColor {
                 // Custom TextColor set
-                btn.setTitleColor(customTextColor, for:UIControl.State())
+                btn.setTitleColor(customTextColor, for: .normal)
             } else {
                 // Use default BackgroundColor derived from AlertStyle
-                btn.setTitleColor(UIColorFromRGB(colorTextButton ?? 0xFFFFFF), for:UIControl.State())
+                btn.setTitleColor(UIColorFromRGB(colorTextButton ?? 0xFFFFFF), for: .normal)
             }
         }
         
@@ -977,7 +977,7 @@ open class SCLAlertView: UIViewController {
 
             let timeoutStr: String = showTimeout.prefix + String(Int(timeout.value)) + showTimeout.suffix
             let txt = String(btn.initialTitle) + " " + timeoutStr
-            btn.setTitle(txt, for: UIControl.State())
+            btn.setTitle(txt, for: .normal)
             
         }
 
